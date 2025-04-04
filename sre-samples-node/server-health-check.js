a7ad6bb97f1be0138bf4b77fc992153e84ddc50e
Thainá França <thainafdamasio@gmail.com>
2025-04-04 20:22:24 +0000

    Execução do quinto exemplo

---
+++ sre-samples-node/server-health-check.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+// Simulando o estado da aplicação para o Readiness Check
+let isReady = false;
+
+// Endpoint Liveness Check - verifica se o servidor está rodando
+app.get('/health/liveness', (req, res) => {
+    res.status(200).send('Liveness check passed');
+});
+
+// Endpoint Readiness Check - verifica se a aplicação está pronta para receber requisições
+app.get('/health/readiness', (req, res) => {
+    if (isReady) {
+        res.status(200).send('Readiness check passed');
+    } else {
+        res.status(503).send('Service is not ready yet');
+    }
+});
+
+// Endpoint para simular a aplicação ficando pronta
+app.get('/make-ready', (req, res) => {
+    isReady = true;
+    res.send('Application is now ready to accept requests');
+});
+
+// Iniciando o servidor
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+});
